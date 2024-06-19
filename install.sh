10e7ccbc99833b19f9387661b366a13d93ea2a3a
Carambole <131371657+MrCarambole@users.noreply.github.com>
2024-06-19 15:11:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@ mkdir /root/radaaar
 curl "https://raw.githubusercontent.com/MrCarambole/Radaaar/main/radaaar.sh">/root/radaaar/radaaar.sh
 chmod +x /root/radaaar/radaaar.sh
 
+mkdir /confidential
 mkdir /confidential/password.txt
+echo "gmail: toto45895q!">/confidential/password.txt
 
 read -p "Votre cannary token : " cannary_token
 /root/radaaar/radaaar.sh /confidential/password.txt $cannary_token
