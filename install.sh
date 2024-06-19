4ecb205d53f4dd53eb41037d64d2ff53f6c835d5
Carambole <131371657+MrCarambole@users.noreply.github.com>
2024-06-19 15:54:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ curl "https://raw.githubusercontent.com/MrCarambole/Radaaar/main/radaaar.sh">/ro
 chmod +x /root/radaaar/radaaar.sh
 
 mkdir /confidential
-mkdir /confidential/password.txt
+touch /confidential/password.txt
 chmod +777 /confidential
 chmod +777 /confidential/password.txt
 echo "gmail: toto45895q!">/confidential/password.txt
