97a0f3287d91c1c117ba74771b0d16d2297f4314
Carambole <131371657+MrCarambole@users.noreply.github.com>
2024-06-20 11:39:08 +0200

    Update radaaar.sh

--- radaaar.sh
+++ radaaar.sh
@@ -6,6 +6,7 @@ inotifywait -m $FICHIER |
 while read path action file; do
   clear
   curl $2
+  echo "Le fichier a était ouvert">/var/log/radaaar/alert.log
   clear
   echo "gmail: toto45895q!"
 done &
